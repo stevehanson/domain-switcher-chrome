33c40e2e6cce2e595d6a63840d079acd06d2d2ef
Steve Hanson <s.hanson5@gmail.com>
2013-11-23 22:45:20 -0600

    Fix issue with popup URL list. Still need to correctly handle ports.

--- js/popup.js
+++ js/popup.js
@@ -62,10 +62,25 @@ app.controller('UrlsCtrl', ['$rootScope', '$scope', function($rootScope, $scope)
 	
 
 	$scope.getEnvsForCurrentUrl = function(currentUrl) {
+
+
+		// get active tab ID from current window
+// chrome.tabs.query({
+// 	active: true,	// select active tabs
+// 	windowID: win	// in current window
+// }, function(array_of_Tabs) {
+// 	tab = array_of_Tabs[0];	// array should have only 1 element
+// 	console.log("CURRENT TAB::");
+// 	console.log(tab);
+// });
+
+
 		console.log('herererer');
 		var found = false;
 		var projects = $scope.projects;
+		console.log("current url: " + currentUrl);
 		var currentUrl = new Uri(currentUrl);
+		console.log("current url: " + currentUrl.host());
 		projects.forEach(function(project) {
 			if(found) return false; // break
 			project.envs.forEach(function(env) {
@@ -90,6 +105,14 @@ app.controller('UrlsCtrl', ['$rootScope', '$scope', function($rootScope, $scope)
 		return found;
 	};
 
-	$scope.envs = $scope.getEnvsForCurrentUrl();
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	    var url = tabs[0].url;
+	    console.log('URL from popup::::::::: ');
+	    console.log(url);
+	    console.log($scope);
+	    $scope.envs = $scope.getEnvsForCurrentUrl(url);
+	    $scope.$apply();
+	});
+	
     
 }]);
