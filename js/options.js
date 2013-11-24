5b483167fbc4f9bdbf4b656eb53c69bb8803afe1
Steve Hanson <s.hanson5@gmail.com>
2013-11-23 19:21:12 -0600

    Most of options page UI set up. Can add/remove project, add environments to projects. Using Angular.

--- js/options.js
+++ js/options.js
@@ -7,3 +7,59 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 });
+
+var app = angular.module('angOptions', []);
+
+app.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+});
+
+
+app.controller('ProjectsCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
+	$scope.projects = [
+	{
+		name: 'Chairs',
+		envs: [
+			{
+				url: 'http://localhost:8080'
+			},
+			{
+				url: 'http://test.chairs.com'
+			},
+			{
+				url: 'http://chairs.com'
+			},
+		]
+	},
+	{
+		name: 'Coffee App',
+		envs: [
+			{
+				url: 'http://localhost:3000'
+			},
+			{
+				url: 'http://test.coffee.com'
+			},
+			{
+				url: 'http://coffee.com'
+			},
+		]
+	}
+	];
+
+	$scope.addEnv = function(project) {
+		project.envs.push({url: ''});
+	}
+
+	$scope.addProject = function(project) {
+		$scope.projects.unshift({ name: 'New Project', envs: [ { url: '' }]});
+	}
+
+	$scope.removeProject = function($index) {
+		console.log($index);
+		$scope.projects.splice($index, 1);
+	}
+    
+}]);
