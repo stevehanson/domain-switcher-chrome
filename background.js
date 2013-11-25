f25666323d9bd8368fa70b3bb4e7e35feac5a170
Steve Hanson <s.hanson5@gmail.com>
2013-11-25 00:26:33 -0600

    Now respects URLs with path in them.

--- background.js
+++ background.js
@@ -27,8 +27,10 @@ if (currVersion != prevVersion) {
 
 // Called when new tab opened
 function checkForValidUrl(tabId, changeInfo, tab) {
-  projects = JSON.parse(localStorage['domainSwitcher']);
-  envs = getEnvsForCurrentUrl(projects, tab.url);
+  var data =localStorage['domainSwitcher'];
+  if(!data) return false; // no projects started
+  var projects = JSON.parse(data);
+  var envs = getEnvsForCurrentUrl(projects, tab.url);
   if(envs) {
 
 	chrome.pageAction.show(tabId);
@@ -48,10 +50,7 @@ function getEnvsForCurrentUrl(projects, currentUrl) {
 	projects.forEach(function(project) {
 		if(found) return false; // break
 		project.envs.forEach(function(env) {
-			var urlToMatch = env.url;
-			if(env.url.indexOf('://') == -1) {
-				urlToMatch = 'http://' + env.url;
-			}
+			var urlToMatch = addHttpIfNoProtocol(env.url);
 			env = new Uri(urlToMatch);
 			if(urlsMatch(currUrl, env)) {
 				found = project.envs.map(function(env){
@@ -68,9 +67,24 @@ function getEnvsForCurrentUrl(projects, currentUrl) {
 
 }
 
+function getCurrentUrlEntry(currentUrl) {
+	var found = null;
+	var currUrl = new Uri(currentUrl);
+	var projects = JSON.parse(localStorage['domainSwitcher']);
+	var envs = getEnvsForCurrentUrl(projects, currentUrl);
+	envs.forEach(function(env) {
+		var urlToMatch = addHttpIfNoProtocol(env.url);
+		env = new Uri(urlToMatch);
+		if(urlsMatch(currUrl, env)) {
+			found = env;
+			return false; // break		
+		}
+	});
+	return found;
+}
 
 function urlsMatch(currUrl, match) {
-	console.log('Checking ' + currUrl.toString() + ' against domain ' + match.toString());
+	//console.log('Checking ' + currUrl.toString() + ' against domain ' + match.toString());
 	if(!match.host()) return false; // if blank url
 	match.setProtocol('http'); // don't care abt protocol
 	currUrl.setProtocol('http'); // don't care abt protocol
@@ -82,7 +96,6 @@ function urlsMatch(currUrl, match) {
 		currUrl.path('/'); // in case '/' added in options page, will still match 
 	}
 	return (currUrl.toString().indexOf(match.toString()) === 0);
-
 }
 
 function addHttpIfNoProtocol(url) {
@@ -97,22 +110,29 @@ chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	chrome.tabs.query({active:true},function(tabs){
-		// update current URL to use host, domain, port, start of path of the selected
-		// environment.
+
+		// update current URL to use host, domain, port, start of path of the selected environment.
 		var uri = new Uri(tabs[0].url);
 		var requestUrl = addHttpIfNoProtocol(request.url);
 		var newUri = new Uri(requestUrl);
 
-		console.log('new uri: ' + newUri.toString());
-		console.log("current uri: " + uri.toString());
-		
+		var currUriEntry = getCurrentUrlEntry(tabs[0].url);
+
+		// update uri to use host, port, protocol of selected ENV
 		uri.host(newUri.host());
 		uri.port(newUri.port());
 		uri.protocol(newUri.protocol());
-		//uri.path(newUri.path + uri.path());
-		console.log('new uri, updated: ' + uri.toString());
+		
+		// update uri to use start of path of selected ENV - if path in ENV url
+		var currentPath = uri.path();
+		if(currUriEntry.path() !== '') {
+			currentPath = uri.path().replace(currUriEntry.path(), '');
+		}
+
+		uri.path(newUri.path() + currentPath);
+		
 		
     chrome.tabs.update(tabs[0].id, {url: uri.toString()});
-    return "1";
+    return 1;
   });
 });
