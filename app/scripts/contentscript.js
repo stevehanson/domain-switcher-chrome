9d997b74a3850ddf4fcea0626fea5640f76c1af6
Yoeri Stessens <stessesyoeri@gmail.com>
2015-03-18 00:20:32 +0100

    Add check on the current url

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -5,23 +5,28 @@ console.log('\'Allo \'Allo! Content script');
 var url = '';
 var color = '';
 var bg_color = '';
+var current_url      = window.location.href;
+console.log(current_url);
 
 chrome.runtime.sendMessage({method: "getLocalStorage", key: "domainSwitcher"}, function(response) {
-  var result = JSON.parse(response.data);
-  console.log(result);
-  
-  url = result[0]['envs'][0]['url'];
-  color = result[0]['envs'][0]['color'];
-  bg_color = result[0]['envs'][0]['bg_color'];
+	var result = JSON.parse(response.data);
+	console.log(result);
+
+	url = result[0]['envs'][0]['url'];
+	color = result[0]['envs'][0]['color'];
+	bg_color = result[0]['envs'][0]['bg_color'];
 });
 
 $(document).ready(function() {
-	var element = '<div class="domain-switcher-bar" style="background-color:'
+
+	if (current_url == url) {
+		var element = '<div class="domain-switcher-bar" style="background-color:'
 		+ bg_color
 		+ '; color:'
 		+ color
 		+ '">'
 		+ url
 		+ '</div>'
-	$('body').append(element);
+		$('body').append(element);
+	}
 });
