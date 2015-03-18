ad77215c06702668d0db14d561c4bfab71dbcd68
Yoeri Stessens <stessesyoeri@gmail.com>
2015-03-18 20:57:44 +0100

    Added bar on all used domains

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -5,22 +5,25 @@ console.log('\'Allo \'Allo! Content script');
 var url = '';
 var color = '';
 var bg_color = '';
-// var current_url = window.location.href.replace('https://www', '');
-var current_url = window.location.href.replace(/(https?:\/\/w*.)/g, '').slice(0, -1);
-console.log(current_url);
+var current_url = window.location.href.replace(/(https?:\/\/w*.)/g, '');
 
 chrome.runtime.sendMessage({method: "getLocalStorage", key: "domainSwitcher"}, function(response) {
-	var result = JSON.parse(response.data);
+	var response = JSON.parse(response.data);
+	var envs = response[0]['envs'];
 
-	url = result[0]['envs'][0]['url'];
-	color = result[0]['envs'][0]['color'];
-	bg_color = result[0]['envs'][0]['bg_color'];
+	$.each(envs, function(index, value) {
+		if (current_url.indexOf(value['url']) == 0) {
+			console.log(envs[index]['url']);
+
+			url = envs[index]['url'];
+			color = envs[index]['color'];
+			bg_color = envs[index]['bg_color'];
+		}
+	});
 });
 
 $(document).ready(function() {
-	console.log(url);
-
-	if (current_url == url) {
+	if (current_url.indexOf(url) == 0) {
 		var element = '<div class="domain-switcher-bar" style="background-color:'
 		+ bg_color
 		+ '; color:'
